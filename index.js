624645383103869aa3cee263d5c5c7963ff3821f
Andrew Esteban <andrewesteban1999@gmail.com>
2019-04-27 17:20:48 +1000

    Added main.html

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var app = express();
 port = process.env.PORT || 3030;
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.sendFile(__dirname + "/public/main.html");
 });
 
 var server = app.listen(port, () => {
     console.log("Server is listening to port: " + port);
-})
+});
