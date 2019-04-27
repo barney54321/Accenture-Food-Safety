a3c2946a9d5b043cad3bb60a30f9102c4b52be69
Andrew Esteban <andrewesteban1999@gmail.com>
2019-04-28 02:16:50 +1000

    Added explanatory comments

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
+// Create app for running server
 var express = require("express");
 var app = express();
+var http = require('http').createServer(app);
+
+// Imports the MySQL library
 var mysql = require("mysql");
+
+// Import classes for Ingredients and Products
 var Product = require("./product.js");
 var Ingredient = require("./ingredient.js");
 var Ingredient_JSON = require("./Ingredient_JSON.js");
 var Ingredient_Block = require("./ingredient_block.js");
 var Product_JSON = require("./product_JSON.js");
 const connection = require("./connection.js");
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
 
+// Define port to be either the port given by distributor, or 3030 by default
 port = process.env.PORT || 3030;
 
-// app.get("/", (req, res) => {
-//     res.send("HELLO");
-// })
-
-
+// Main route sends the home page
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/home.html");
 });
 
+// Scanner route sends the QR scanner page, using InstaScan
 app.get("/scanner", (req, res) => {
     res.sendFile(__dirname + "/public/scanner.html");
 });
 
+// Partners route sends the list of partners
 app.get("/partners", (req, res) => {
     res.sendFile(__dirname + "/public/partners.html");
 });
 
-console.log('A user connected');
-// socket.on('disconnect', function(){
-//     console.log('User disconnected');
-// });
+// Settings route sends the settings page
+app.get("/settings", (req, res) => {
+    res.sendFile(__dirname + "/public/settings.html");
+});
 
+// Wildcard for when a user accesses a product page
 app.get("/product/*", (req, res) => {
+    // Response is the template main page that gets updated
+    // by an internal jQuery getJSON method
     res.sendFile(__dirname+"/public/main.html");
 })
 
+// This sends the data related to the product ID
 app.get("/data/*", (req, res) => {
     var test = new Ingredient_JSON("Apple", "Peru", ["Farmer Joe", "Harris Farm"], 1234, ["Nut", "Halal"]);
     var test1 = new Ingredient_JSON("Pear", "Austria", ["Farmer Bob", "IGA"], 4321, ["Vegan", "Halal"]);
@@ -139,10 +146,7 @@ app.get("/sdfj/*", (req, res) => {
     });
 });
 
-app.get("/sample", (req, res) => {
-    res.sendFile(__dirname + "/public/main.html");
-})
-
+// Sets the server to listen to port
 http.listen(port, () => {
     console.log("Server is listening to port: " + port);
 });
